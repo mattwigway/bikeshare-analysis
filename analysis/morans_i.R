ddc49afd0096597a17899e39bfb1b7cf42e5163a
mattwigway <mattwigway@gmail.com>
2013-12-02 10:44:51 -0800

    Add labels.

--- analysis/morans_i.R
+++ analysis/morans_i.R
@@ -1,5 +1,18 @@
 # Calculate Moran's I for bike share data; are station popularities autocorrelated?
-# Copyright (C) 2013 Matthew Wigginton Conway. All rights reserved.
+
+# Copyright (C) 2013 Matthew Wigginton Conway.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 TIMEZONE <- 'America/New_York'
 BREAK_LINKS_OVER <- 4000 # 4000 m, empirically determined; much less than 4000m and we start seeing more than
--- analysis/periods.R
+++ analysis/periods.R
@@ -1,8 +1,19 @@
 # Time periods
-# Copyright (C) 2013 Matthew Wigginton Conway. All rights reserved.
 
-# A few functions for labeling
-# This one labels based on time of day.
+# Copyright (C) 2013 Matthew Wigginton Conway.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # We use the same time-of-day spans as used by MWCOG in their four-step model, page 14:
 # Metropolitan Washington Council of Governments. User's Guide for the MWCOG/NCRTPB Travel
 # Forecasting Model, Version 2.3, Build 52: Draft Report. Washington, 2013.
